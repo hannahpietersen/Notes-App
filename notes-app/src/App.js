45606742f8ee883c0b558992b8079925980d1a2a
someid <hannahpietersen@gmail.com>
2023-03-29 18:56:31 +0530

    final commit

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -10,11 +10,12 @@ export default function App() {
     
     
     const [notes, setNotes] = React.useState(
-        JSON.parse(localStorage.getItem("notes")) || [])
+        () => JSON.parse(localStorage.getItem("notes")) || []
+    )
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
-    const [state, setState] = React.useState(console.log("State initialization"))
+
 
   // adding item to local storage
     React.useEffect(() => {
@@ -31,14 +32,36 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
+        //Rearranging the edited note to the top of the list
+        setNotes(oldNotes => {
+            const newArray = []
+            for(let i=0; i<oldNotes.length; i++) {
+                const oldNote = oldNotes[i]
+                if(oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+    }
+
+    //This does not rearrange the notes, old notes are just placed at the top
+    /* setNotes(oldNotes => oldNotes.map(oldNote => {
             return oldNote.id === currentNoteId
                 ? { ...oldNote, body: text }
                 : oldNote
-        }))
-    }
+        })) */
     
-    function findCurrentNote() {
+
+        function deleteNote(event, noteId) {
+            event.nativeEvent.stopImmediatePropagation();
+            setNotes(oldNotes => oldNotes.filter((note) => note.id !== noteId))
+        }
+    
+    
+        function findCurrentNote() {
         return notes.find(note => {
             return note.id === currentNoteId
         }) || notes[0]
@@ -59,6 +82,7 @@ export default function App() {
                     currentNote={findCurrentNote()}
                     setCurrentNoteId={setCurrentNoteId}
                     newNote={createNewNote}
+                    deleteNote={deleteNote}
                 />
                 {
                     currentNoteId && 
